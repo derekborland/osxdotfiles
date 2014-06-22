771ce3812669c2315716efd60d6c149333131f92
Derek Borland <derekborland@gmail.com>
2014-06-21 20:00:30 -0700

    npm setup

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #! /usr/bin/env node
 
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .usage('[options] [command]')
+  .option('-t, --test', 'Test')
+  .parse(process.argv);
+
+/**
+ * If no args are passed in, return `help()`
+ * 
+ */
+if(program.rawArgs.length == 2) {
+  program.help();
+}
